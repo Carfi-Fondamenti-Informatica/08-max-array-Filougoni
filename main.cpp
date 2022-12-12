465f8cf0fb123a199c910f80a8669732402c10c8
Filougoni <116490094+Filougoni@users.noreply.github.com>
2022-12-12 19:47:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
-#include <iostream>
 
+#include <iostream>
+#include "lib.h"
+using namespace std;
 int main(){
-  return 0;
+    int dimensione;
+   cin>> dimensione;
+   float numeri[dimensione];
+    for (int i = 0; i < dimensione; i++) {
+        cin >> numeri[i];
+    }
+    cout << funzione(numeri, dimensione)<<endl;
+    return 0;
 }

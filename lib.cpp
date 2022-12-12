97629c59c45d2a9d57bd8592936f69605c2d44c9
Filougoni <116490094+Filougoni@users.noreply.github.com>
2022-12-12 19:41:35 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
 
+
+#include <iostream>
+#include "lib.h"
+using namespace std;
+float funzione(float array[],int dimensione){
+    float max= array[0];
+    for(int i=1;i<dimensione;i++){
+        if(max<array[i])
+            max=array[i];
+    }
+    return max;
+}
